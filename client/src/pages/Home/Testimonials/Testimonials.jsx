093c4cfc1273a3de69f6778ed63149fed53522a5
Arefin Aareef <arefin.aareef@gmail.com>
2023-08-16 00:00:22 +0600

    testimonial complete

--- client/src/pages/Home/Testimonials/Testimonials.jsx
+++ client/src/pages/Home/Testimonials/Testimonials.jsx
@@ -4,33 +4,37 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const Testimonials = () => {
-    const [reviews, setReviews] = useState([]);
-    useEffect(() => {
-        fetch('review.json')
-        .then(res => res.json())
-        .then(data => setReviews(data))
-    },[])
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("review.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
 
   return (
-    <section>
+    <section className="p-4">
       <SectionTitle
         subHeading="What Our Client Says"
         heading="Testimonials"
+        textColor="slate"
       ></SectionTitle>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        {
-            reviews.map(review => <SwiperSlide
-                key={review._id}
-            >
-                <div className="m-24">
-                    <p>{review.details}</p>
-                    <p>{review.name}</p>
-                </div>
-            </SwiperSlide>)
-        }
+      <div>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper" style={{ padding: 0, height: 200}}>
+        {reviews.map((review) => 
+          <SwiperSlide key={review._id}>
+            <div className="flex flex-col items-center md:pt-6 ">
+              <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+              <p className="text-xl mx-10 text-center py-2">{review.details}</p>
+              <p className="text-3xl font-semibold text-slate-500">{review.name}</p>
+            </div>
+          </SwiperSlide>
+        )}
       </Swiper>
+      </div>
     </section>
   );
 };
