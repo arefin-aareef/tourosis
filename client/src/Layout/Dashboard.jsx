c2a16c0f959341daca8a194cc4b3ddb4c48a513d
Arefin Aareef <arefin.aareef@gmail.com>
2023-09-25 00:36:33 +0600

    delete from cart

--- client/src/Layout/Dashboard.jsx
+++ client/src/Layout/Dashboard.jsx
@@ -1,10 +1,12 @@
 import {  NavLink, Outlet } from "react-router-dom";
 import { FaBars, FaCalendarAlt, FaHome, FaMailBulk, FaShoppingBag, FaShoppingCart, FaWallet } from 'react-icons/fa';
+import useCart from "../hooks/useCart";
 
 
 const Dashboard = () => {
+  const [cart] = useCart()
   return (
-    <div>
+    <div className="">
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center bg-slate-300">
@@ -23,7 +25,7 @@ const Dashboard = () => {
           <ul className="menu p-4 w-80 min-h-full bg-slate-400 ">
             {/* Sidebar content here */}
             <li><NavLink to="/dashboard/home"><FaHome />User Home</NavLink></li>
-            <li><NavLink to="/dashboard/mycart"><FaShoppingCart />My Cart</NavLink></li>
+            <li><NavLink to="/dashboard/mycart"><FaShoppingCart />My Cart <div className="inline badge badge-secondary">+{cart?.length || 0}</div></NavLink></li>
             <li><NavLink to="/dashboard/reservation"><FaCalendarAlt />Reservations</NavLink></li>
             <li><NavLink to="/dashboard/payment"><FaWallet />Payment History</NavLink></li>
             <div className="divider"></div> 
--- client/src/pages/Dashboard/MyCart/MyCart.jsx
+++ client/src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,12 +1,96 @@
 import { Helmet } from "react-helmet-async";
+import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from 'sweetalert2'
+
 
 const MyCart = () => {
+  const [cart, refetch] = useCart()
+  console.log(cart);
+  const total = cart.reduce((sum, item) => item.price + sum, 0)
+
+  const handleDelete = item => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+      customClass: {
+        popup: 'custom-swal-popup' // Add a custom CSS class
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/carts/${item._id}`, {
+          method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+          if (data.deletedCount > 0) {
+            refetch()
+            Swal.fire(
+              'Deleted!',
+              'Your file has been deleted.',
+              'success'
+            )
+          }
+        })
+      }
+    })
+  }
+
   return (
-    <div>
+    <div className="w-full">
       <Helmet>
         <title>Tourosis | My Cart</title>
       </Helmet>
-      my cart
+      <div className="uppercase font-semibold h-[60px] items-center flex justify-evenly">
+        <h3>Total Bookings: {cart.length}</h3>
+        <h3>Total Price: ${total}</h3>
+        <button className="btn btn-warning btn-sm">Pay</button>
+      </div>
+      <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Tour</th>
+        <th>Details</th>
+        <th>Price</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        cart.map((item, index) =>       <tr key={item._id}>
+          <td>
+            {index + 1}
+          </td>
+          <td>
+              <div className="avatar">
+                <div className="mask mask-squircle w-12 h-12">
+                  <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                </div>
+              </div>
+          </td>
+          <td>
+           {item.tour_name}
+            <br/>
+            <span className="badge badge-ghost badge-sm">{item.tour_details}</span>
+          </td>
+          <td className="text-end">${item.price}</td>
+          <td>
+            <button onClick={() => handleDelete(item)} className="btn btn-ghost bg-red-600 text-white"><FaTrashAlt /> </button>
+          </td>
+        </tr>)
+      }
+
+    </tbody>    
+  </table>
+</div>
     </div>
   );
 };
