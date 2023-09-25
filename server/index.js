a3b04ec0cf00536ecae2b31a0f758d3ced895ce2
Arefin Aareef <arefin.aareef@gmail.com>
2023-09-26 02:54:53 +0600

    axios interceptor

--- server/index.js
+++ server/index.js
@@ -9,6 +9,20 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorize access'})
+  }
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorize access'})
+    }
+    req.decoded = decoded
+    next()
+  })
+} 
 
 // mongodb
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -81,10 +95,16 @@ async function run() {
         res.send(result)
     })
 
-    app.get('/carts', async(req, res) => {
+    // carts apis
+
+    app.get('/carts', verifyJWT, async(req, res) => {
         const email = req.query.email
         if(!email){
           res.send([])
+        }
+        const decodedEmail = req.decoded.email;
+        if(email !== decodedEmail){
+          return res.status(403).send({error: true, message: 'forbidden access'})
         }
         const query = { email: email}
         const result = await cartCollection.find().toArray()
